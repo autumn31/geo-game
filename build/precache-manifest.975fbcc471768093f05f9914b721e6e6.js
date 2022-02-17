ebd8ea90ef8356d0b73c26c3638b1055852daa64
autumn31 <autumn.forever31@gmail.com>
2022-02-18 00:33:33 +0800

    add build

---
+++ build/precache-manifest.975fbcc471768093f05f9914b721e6e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c5d61ee8f5ae87e1efa",
+    "url": "/static/css/main.ebeb3cc8.chunk.css"
+  },
+  {
+    "revision": "5c5d61ee8f5ae87e1efa",
+    "url": "/static/js/main.5c5d61ee.chunk.js"
+  },
+  {
+    "revision": "e80b766163af02c8be6c",
+    "url": "/static/css/1.77b6cf2e.chunk.css"
+  },
+  {
+    "revision": "e80b766163af02c8be6c",
+    "url": "/static/js/1.e80b7661.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "e08c9b0d517a644fa818a7e130fd8d7a",
+    "url": "/index.html"
+  }
+];
