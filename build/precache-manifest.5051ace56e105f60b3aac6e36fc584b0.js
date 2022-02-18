f7afb7ee3daf1fd3e34a826fa75e27a29504b2e7
autumn31 <autumn.forever31@gmail.com>
2022-02-18 08:40:27 +0800

    rebuild with correct homepage

---
+++ build/precache-manifest.5051ace56e105f60b3aac6e36fc584b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d719790f522e69409fe8",
+    "url": "/geo-game/build/static/css/main.d75d882d.chunk.css"
+  },
+  {
+    "revision": "d719790f522e69409fe8",
+    "url": "/geo-game/build/static/js/main.d719790f.chunk.js"
+  },
+  {
+    "revision": "e80b766163af02c8be6c",
+    "url": "/geo-game/build/static/css/1.77b6cf2e.chunk.css"
+  },
+  {
+    "revision": "e80b766163af02c8be6c",
+    "url": "/geo-game/build/static/js/1.e80b7661.chunk.js"
+  },
+  {
+    "revision": "24c4b41b871229ce0131",
+    "url": "/geo-game/build/static/js/runtime~main.24c4b41b.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/geo-game/build/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "118a2a1f837675bcc6394c302a91a0ae",
+    "url": "/geo-game/build/index.html"
+  }
+];
