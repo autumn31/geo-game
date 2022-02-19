a27e1883278c684cf9e22b753f1c0225ffe52fd5
autumn31 <autumn.forever31@gmail.com>
2022-02-19 09:55:37 +0800

    feat: add real question

--- src/App.js
+++ src/App.js
@@ -6,27 +6,26 @@ import "./App.css";
 import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
-const acceptRange = 50;
+const acceptRange = 12;
+const byPassSecret = "autumn31 is smart";
 
 function distance(lat1, lon1, lat2, lon2) {
-  if (lat1 == lat2 && lon1 == lon2) {
-    return 0;
-  } else {
-    var radlat1 = (Math.PI * lat1) / 180;
-    var radlat2 = (Math.PI * lat2) / 180;
-    var theta = lon1 - lon2;
-    var radtheta = (Math.PI * theta) / 180;
-    var dist =
-      Math.sin(radlat1) * Math.sin(radlat2) +
-      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-    if (dist > 1) {
-      dist = 1;
-    }
-    dist = Math.acos(dist);
-    dist = (dist * 180) / Math.PI;
-    dist = dist * 60 * 1.1515;
-    return dist;
-  }
+  var R = 6371; // km
+  var dLat = toRad(lat2 - lat1);
+  var dLon = toRad(lon2 - lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d * 1000;
+}
+
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
 }
 class App extends Component {
   constructor(props) {
@@ -64,6 +63,9 @@ class App extends Component {
     const { location, text } = this.state.answer;
     console.log(pos, userAnswer);
     console.log(location, text);
+    if (userAnswer === byPassSecret) {
+      return true;
+    }
     if (
       location &&
       distance(pos[0], pos[1], location[0], location[1]) > acceptRange
@@ -127,7 +129,7 @@ class App extends Component {
   }
 
   ontextChange(e) {
-    this.setState({ answer: e.target.value });
+    this.setState({ userAnswer: e.target.value });
   }
 
   renderQuiz() {
--- src/components/InputAnswer.js
+++ src/components/InputAnswer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./style.css";
 
 function InputAnswer(props) {
+  console.log(props.answer);
   return !props.answer ? (
     <div />
   ) : (
-    <div>
-      <textarea onChange={props.ontextChange} />
+    <div class="centered">
+      <textarea
+        onChange={props.ontextChange}
+        defaultValue={props.answer.text ? "" : "此題不用答案"}
+      />
       <button onClick={props.handleSubmit}>Submit</button>
     </div>
   );
